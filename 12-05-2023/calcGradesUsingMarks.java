7994bc5c1ae60e5d210e26abc9a9ddc7a0d9a64d
mr-selviraja <i.am.selviraja.p@gmail.com>
2023-05-25 17:29:33 +0530

    Calculate grades using Marks

---
+++ 12-05-2023/calcGradesUsingMarks.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class calcGradesUsingMarks {
+    public static void main(String args[]) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your marks to get grade: ");
+        int marks = input.nextInt();
+
+        if (marks<0 || marks>100) {
+            System.out.println("Invalid marks");
+            return;
+        }
+
+        if (marks >= 60)
+            System.out.println("Passed with " + marks + " marks.")
+        else if(marks >= 50 || marks < 60)
+            System.out.println("Marginally passed with " + marks + " marks.")
+        else
+            System.out.println("Failed with " + marks + " marks.");
+    }
+}
