a1f78613e848e2454c958f9ec0ef649c366910eb
mr-selviraja <i.am.selviraja.p@gmail.com>
2023-05-25 17:49:17 +0530

    Calculate grades using Marks[Bug Fixed]

--- 12-05-2023/calcGradesUsingMarks.java
+++ 12-05-2023/calcGradesUsingMarks.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class calcGradesUsingMarks {
     public static void main(String args[]) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter your marks to get grade: ");
+        System.out.print("Enter your marks(0-100) to get grade: ");
         int marks = input.nextInt();
 
         if (marks<0 || marks>100) {
@@ -12,9 +12,9 @@ public class calcGradesUsingMarks {
         }
 
         if (marks >= 60)
-            System.out.println("Passed with " + marks + " marks.")
-        else if(marks >= 50 || marks < 60)
-            System.out.println("Marginally passed with " + marks + " marks.")
+            System.out.println("Passed with " + marks + " marks.");
+        else if(marks >= 50 && marks < 60)
+            System.out.println("Marginally passed with " + marks + " marks.");
         else
             System.out.println("Failed with " + marks + " marks.");
     }
